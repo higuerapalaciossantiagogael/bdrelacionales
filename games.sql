215bb19e3de5f842d5ee4e4ee821d1a16fbbace1
Higuera Palacios Santiago Gael <wisrexmc@gmail.com>
2026-04-29 08:11:10 -0700

    Add files via upload

---
+++ games.sql
@@ -0,0 +1,158 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 29-04-2026 a las 17:09:51
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `games`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `genero`
+--
+
+CREATE TABLE `genero` (
+  `id_genero` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `descripcion` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `genero`
+--
+
+INSERT INTO `genero` (`id_genero`, `nombre`, `descripcion`) VALUES
+(10, 'Plataformas', 'Juegos de estilo plataformero clásico.'),
+(11, 'FPS', 'Juegos de disparos en primera persona.'),
+(12, 'Sandbox', 'Explora, crea, interactua con usuarios en linea.'),
+(14, 'Metroidvania', 'Juegos de exploracion no lineal en 2D.'),
+(15, 'Simulacion', 'Juegos de Inmersion realista en entornos digitales.');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `plataforma`
+--
+
+CREATE TABLE `plataforma` (
+  `id_plataforma` int(11) NOT NULL,
+  `nombre` varchar(60) NOT NULL,
+  `fabricante` varchar(80) NOT NULL,
+  `anio_lanzamiento` year(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `plataforma`
+--
+
+INSERT INTO `plataforma` (`id_plataforma`, `nombre`, `fabricante`, `anio_lanzamiento`) VALUES
+(3, 'Pretendo', 'Pretendo', '1967'),
+(5, 'XCaja', 'Microsuave', '1987'),
+(6, 'Estacion de juegos', 'Sonya', '2000');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `videojuego`
+--
+
+CREATE TABLE `videojuego` (
+  `id_juego` int(11) NOT NULL,
+  `titulo` varchar(120) NOT NULL,
+  `desarrolladora` varchar(100) NOT NULL,
+  `anio_lanzamiento` year(4) NOT NULL,
+  `precio` decimal(6,2) NOT NULL,
+  `id_genero` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `videojuego`
+--
+
+INSERT INTO `videojuego` (`id_juego`, `titulo`, `desarrolladora`, `anio_lanzamiento`, `precio`, `id_genero`) VALUES
+(1, 'Super Carlos 64', 'Pretendo', '1986', 799.00, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `videojuego_plataforma`
+--
+
+CREATE TABLE `videojuego_plataforma` (
+  `id_juego` int(11) NOT NULL,
+  `id_plataforma` int(11) NOT NULL,
+  `fecha_disponible` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `genero`
+--
+ALTER TABLE `genero`
+  ADD PRIMARY KEY (`id_genero`);
+
+--
+-- Indices de la tabla `plataforma`
+--
+ALTER TABLE `plataforma`
+  ADD PRIMARY KEY (`id_plataforma`);
+
+--
+-- Indices de la tabla `videojuego`
+--
+ALTER TABLE `videojuego`
+  ADD PRIMARY KEY (`id_juego`),
+  ADD KEY `id_genero` (`id_genero`);
+
+--
+-- Indices de la tabla `videojuego_plataforma`
+--
+ALTER TABLE `videojuego_plataforma`
+  ADD KEY `id_juego` (`id_juego`),
+  ADD KEY `id_plataforma` (`id_plataforma`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `genero`
+--
+ALTER TABLE `genero`
+  MODIFY `id_genero` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT de la tabla `plataforma`
+--
+ALTER TABLE `plataforma`
+  MODIFY `id_plataforma` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT de la tabla `videojuego`
+--
+ALTER TABLE `videojuego`
+  MODIFY `id_juego` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
